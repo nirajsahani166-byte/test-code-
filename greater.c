5b0471c76f9f8bb47e2b8352abe9d00e90d38924
nirajsahani166-byte <nirajsahani166@gmail.com>
2025-12-04 13:20:30 +0545

    more efficient program

--- greater.c
+++ greater.c
@@ -4,15 +4,15 @@ int main()
 int a,b;
 printf("enter the two numbers");
 scanf("%d%d",&a,&b);
-if(a>b)
+if(a>b&&b<a)
 {
-    printf("%d is greater",a);
+    printf("%d is greater \n%d is smaller",a,b);
 }
-else if(b>a)
+else if(b>a&&a<b)
 {
-    printf("%d is greater",b );
+    printf("%d is greater \n%d is smaller",b,a );
 }
-else if(a==b&&b==a)
+else if(a==b)
 {
     printf("%d is equal numbers",a);
 }
