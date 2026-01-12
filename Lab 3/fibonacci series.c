569d95215624602f1294fe3e3ef7fb5823ecca5c
nirajsahani166-byte <nirajsahani166@gmail.com>
2026-01-12 16:12:35 +0545

    more improved code

--- Lab 3/fibonacci series.c
+++ Lab 3/fibonacci series.c
@@ -2,15 +2,17 @@
 #include<math.h>
 int main()
 {
-    int n, t1=1,t2=1,nxt;
+    int n, t1=1,t2=1,nxt,s=0;
     printf("Enter the number of terms");
     scanf("%d",&n);
     for(int i=1;i<=n;i++)
-    {
+    { 
+        s=s+t1;
         printf( "%d\n",t1);
         nxt=t1+t2;
         t1=t2;
         t2=nxt;
     }
+    printf("Sum of series is %d",s);
     return 0;
 }
