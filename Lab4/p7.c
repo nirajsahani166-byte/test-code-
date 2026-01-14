4169d98a89d37ddc28178ed71274d07fcc4b537c
nirajsahani166-byte <nirajsahani166@gmail.com>
2026-01-14 19:48:20 +0545

    debugging

--- Lab4/p7.c
+++ Lab4/p7.c
@@ -2,17 +2,18 @@
 #include<math.h>
 int main()
 {
-    int n, i,x,s=0 ,t1;
+    int n, i,x ,t1;
+    float s=0;
     printf("enter the term");
     scanf("%d",&n);
     printf("enter the value of x");
     scanf("%d",&x);
-    for(i=1;i<n;i++)
+    for(i=1;i<=n;i++)
     {
-        t1=pow(x/i,i);
+        t1=pow(x,i)/i;
     printf("%d\n",t1);
     s=s+t1;
     }
-    printf("sum of series is %d",s);
+    printf("sum of series is %f",s);
     return 0;
 }
